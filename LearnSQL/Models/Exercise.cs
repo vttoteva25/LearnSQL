42a3fe5788e5757e3180fa7c644902479ca3f8f8
Valeria Stamenova <valeria.stamenova3@gmail.com>
2022-03-30 11:53:05 +0300

    Adding Angel's models with conventions modifications

---
+++ LearnSQL/Models/Exercise.cs
@@ -0,0 +1,16 @@
+﻿namespace LearnSQL.Models
+{
+	public class Exercise
+	{
+		public Exercise(object id, object problem, object solution)
+		{
+			this.Id = (int)id;
+			this.Problem = (string)problem;
+			this.Solution = (string)solution;
+		}
+
+		public int Id { get; set; }
+		public string Problem { get; set; }
+		public string Solution { get; set; }
+	}
+}
---
+++ LearnSQL/Models/MaterialExercise.cs
@@ -0,0 +1,17 @@
+﻿namespace LearnSQL.Models
+{
+	public class MaterialExercise
+    {
+        public MaterialExercise(object matId, object exId)
+        {
+            this.MaterialId = (int)matId;
+            this.ExerciseId = (int)exId;
+        }
+
+        public int MaterialId { get; set; }
+        public Material Material { get; set; }
+
+        public int ExerciseId { get; set; }
+        public Exercise Exercise { get; set; }
+    }
+}
