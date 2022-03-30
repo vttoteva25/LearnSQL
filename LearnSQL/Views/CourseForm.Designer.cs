6f177523e6e05e25f4d8035d9723f3c263cdb8cf
Valeria Stamenova <valeria.stamenova3@gmail.com>
2022-03-30 16:13:19 +0300

    Change list to buttons

--- LearnSQL/Views/CourseForm.Designer.cs
+++ LearnSQL/Views/CourseForm.Designer.cs
@@ -32,8 +32,15 @@ namespace LearnSQL
 			this.MaterialsRichTextBox = new System.Windows.Forms.RichTextBox();
 			this.StagePanel = new System.Windows.Forms.Panel();
 			this.panel1 = new System.Windows.Forms.Panel();
-			this.StagesListBox = new System.Windows.Forms.ListBox();
 			this.OpenPopUpFormButton = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
 			this.StagePanel.SuspendLayout();
 			this.panel1.SuspendLayout();
 			this.SuspendLayout();
@@ -61,21 +68,18 @@ namespace LearnSQL
 			// panel1
 			// 
 			this.panel1.BackColor = System.Drawing.Color.LightGreen;
-			this.panel1.Controls.Add(this.StagesListBox);
+			this.panel1.Controls.Add(this.button8);
+			this.panel1.Controls.Add(this.button7);
+			this.panel1.Controls.Add(this.button4);
+			this.panel1.Controls.Add(this.button5);
+			this.panel1.Controls.Add(this.button6);
+			this.panel1.Controls.Add(this.button3);
+			this.panel1.Controls.Add(this.button2);
+			this.panel1.Controls.Add(this.button1);
 			this.panel1.Location = new System.Drawing.Point(28, 21);
 			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(292, 584);
+			this.panel1.Size = new System.Drawing.Size(292, 546);
 			this.panel1.TabIndex = 6;
-			// 
-			// StagesListBox
-			// 
-			this.StagesListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.StagesListBox.FormattingEnabled = true;
-			this.StagesListBox.ItemHeight = 26;
-			this.StagesListBox.Location = new System.Drawing.Point(16, 21);
-			this.StagesListBox.Name = "StagesListBox";
-			this.StagesListBox.Size = new System.Drawing.Size(254, 550);
-			this.StagesListBox.TabIndex = 4;
 			// 
 			// OpenPopUpFormButton
 			// 
@@ -83,13 +87,109 @@ namespace LearnSQL
 			this.OpenPopUpFormButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.OpenPopUpFormButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
 			this.OpenPopUpFormButton.ForeColor = System.Drawing.Color.White;
-			this.OpenPopUpFormButton.Location = new System.Drawing.Point(761, 554);
+			this.OpenPopUpFormButton.Location = new System.Drawing.Point(761, 542);
 			this.OpenPopUpFormButton.Name = "OpenPopUpFormButton";
 			this.OpenPopUpFormButton.Size = new System.Drawing.Size(249, 63);
 			this.OpenPopUpFormButton.TabIndex = 5;
 			this.OpenPopUpFormButton.Text = "Към задачите";
 			this.OpenPopUpFormButton.UseVisualStyleBackColor = false;
 			this.OpenPopUpFormButton.Click += new System.EventHandler(this.button1_Click_1);
+			// 
+			// button1
+			// 
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button1.ForeColor = System.Drawing.Color.White;
+			this.button1.Location = new System.Drawing.Point(0, 0);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(292, 70);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button2.ForeColor = System.Drawing.Color.White;
+			this.button2.Location = new System.Drawing.Point(0, 67);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(292, 70);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button3.ForeColor = System.Drawing.Color.White;
+			this.button3.Location = new System.Drawing.Point(0, 134);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(292, 70);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "button3";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button4.ForeColor = System.Drawing.Color.White;
+			this.button4.Location = new System.Drawing.Point(0, 200);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(292, 70);
+			this.button4.TabIndex = 5;
+			this.button4.Text = "button4";
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button5.ForeColor = System.Drawing.Color.White;
+			this.button5.Location = new System.Drawing.Point(0, 264);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(292, 70);
+			this.button5.TabIndex = 4;
+			this.button5.Text = "button5";
+			this.button5.UseVisualStyleBackColor = true;
+			// 
+			// button6
+			// 
+			this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button6.ForeColor = System.Drawing.Color.White;
+			this.button6.Location = new System.Drawing.Point(0, 331);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(292, 70);
+			this.button6.TabIndex = 3;
+			this.button6.Text = "button6";
+			this.button6.UseVisualStyleBackColor = true;
+			// 
+			// button7
+			// 
+			this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button7.ForeColor = System.Drawing.Color.White;
+			this.button7.Location = new System.Drawing.Point(0, 396);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(292, 70);
+			this.button7.TabIndex = 6;
+			this.button7.Text = "button7";
+			this.button7.UseVisualStyleBackColor = true;
+			// 
+			// button8
+			// 
+			this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button8.ForeColor = System.Drawing.Color.White;
+			this.button8.Location = new System.Drawing.Point(0, 462);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(292, 81);
+			this.button8.TabIndex = 7;
+			this.button8.Text = "button8";
+			this.button8.UseVisualStyleBackColor = true;
 			// 
 			// Course
 			// 
@@ -113,8 +213,15 @@ namespace LearnSQL
 
         private System.Windows.Forms.RichTextBox MaterialsRichTextBox;
         private System.Windows.Forms.Panel StagePanel;
-        private System.Windows.Forms.ListBox StagesListBox;
         private System.Windows.Forms.Button OpenPopUpFormButton;
         private System.Windows.Forms.Panel panel1;
-    }
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+	}
 }
