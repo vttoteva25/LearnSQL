596dacfed590ac0af890c0c85f27232d7b828eaa
Valeria Stamenova <valeria.stamenova3@gmail.com>
2022-04-01 12:32:55 +0300

    Delete tables

---
+++ LearnSQL/Database/DeleteTables.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS [dbo].[MaterialsExcercises];
+DROP TABLE IF EXISTS [dbo].[Excercises];
+DROP TABLE IF EXISTS [dbo].[Materials];
+DROP TABLE IF EXISTS [dbo].[Users];
+DROP TABLE IF EXISTS [dbo].[Stages];
